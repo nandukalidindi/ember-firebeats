6a97a7131b6b5ff145e8fdde2d26552fc71c20e2
nandukalidindi <nandukalidindi@gmail.com>
2017-02-17 10:30:07 -0500

    Move the chart options over into the application controller for the planned workflow

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -303,6 +303,59 @@ export default Ember.Controller.extend({
     }
   ],
 
+  chartOptions: {
+      chart: { type: 'spline' },
+      title: { text: '' },
+      subtitle: { text: '' },
+      exporting: { enabled: false },
+      credits: { enabled: false },
+      xAxis: [
+        {
+          title: { text: '' },
+          gridLineWidth: 1,
+          tickInterval: 1
+        },
+        {
+          className: "highcharts-color-1",
+          opposite: true,
+        }
+      ],
+      legend: {
+          enabled: false
+      },
+      yAxis: {
+        title: { text: '' },
+        tickInterval: 60,
+        gridLineWidth: 0,
+        minorTickLength: 0,
+        tickLength: 0
+      },
+      tooltip: {
+        headerFormat: '<b>{series.name}</b><br>',
+        pointFormat: '{point.y} BPM at {point.x}:00'
+      },
+      plotOptions: {
+        series: {
+          // lineWidth: 2,
+        },
+        spline: {
+          marker: {
+              enabled: false
+          }
+        }
+      },
+      lang: {
+          noData: "No data to display"
+      },
+      noData: {
+          style: {
+              fontWeight: 'bold',
+              fontSize: '15px',
+              color: '#303030'
+          }
+      }
+  },
+
   init() {
     this._super(...arguments);
     (this.get('chartData') || []).forEach(function(series) {
